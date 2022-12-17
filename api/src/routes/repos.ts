239ef2ec0c3b747a5f5ff91a3352db8baafadb96
umairzafar164 <umairzafar164@gmail.com>
2022-12-17 18:46:36 -0400

    Api: Aggregated data from sources, filtered by fork status and sent as json response

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,27 @@
 import { Router, Request, Response } from 'express';
+import fetch from 'node-fetch';
+import jsonData from '../../data/repos.json';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
-  res.status(200);
-
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  const url = 'https://api.github.com/users/silverorange/repos';
+  const options = {
+    method: 'GET',
+  };
+  const response = await fetch(url, options)
+    .then((data: any) => data.json())
+    .catch((e: any) => {
+      const error = new Error('Error fetching data');
+      throw error;
+    });
+  if (response.length > 0) {
+    const aggregatedData = response.concat(jsonData);
+    const filteredData = aggregatedData.filter((d: any) => d.fork === false);
+    res.status(200).json(filteredData);
+  } else {
+    res.status(401).json({ message: 'Could not fetch repos data' });
+  }
 });
